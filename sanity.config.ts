175de930f04ad97d6b0b7929c3adacb89be97735
Shahan <shaadashahan@gmail.com>
2026-02-12 00:43:48 +0400

    updated packages

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,9 @@ import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
 
 export const config=defineConfig({
-    projectId:"nta5w95l",
-    dataset:"production",
+    // projectId:"nta5w95l",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, 
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     title:"My Personal Website",
     apiVersion:"2024-08-16",
     basePath:"/admin",
--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,6 +1,6 @@
 const clientConfig={
-    projectId:"nta5w95l",
-    dataset:"production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     apiVersion:"2024-08-16",
 }
 export default clientConfig;
